868b5620f704871eecdf381a8bde25b348d95479
Ronja <axoila@gmx.de>
2017-08-02 15:21:52 +0200

    project

---
+++ Assets/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    [SerializeField] float speed;
+    Rigidbody rigid;
+
+    // Use this for initialization
+    void Start () {
+        rigid = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rigid.MovePosition(rigid.position + transform.forward * Time.deltaTime * speed);
+    }
+}
---
+++ Assets/SetCameraPosition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SetCameraPosition : MonoBehaviour {
+    [SerializeField] Transform player;
+    [SerializeField] Vector3 offset;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = player.position + offset;
+    }
+}
